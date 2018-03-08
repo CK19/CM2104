02817d8baa9b81d0c9f893aa9d147b8c53850e80
1605819 <c.kennedy5@rgu.ac.uk>
2018-03-08 09:48:13 +0000

    jeff

--- Lab6/form.js
+++ Lab6/form.js
@@ -1,25 +1,26 @@
 function main () {
-	var a b
+	var a;
+	var b;
 	var c;
-	
+
 	a = 10;
 	b = 100;
-	
-	b = + a b;
-	c == a * b;
-	
+
+	b += a;
+	c = a * b;
+
 	document.writeln(c);
 }
 
 // function main () {
 // 	var a, b;
 // 	var c;
-// 	
+//
 // 	a = 10;
 // 	b = 100;
-// 	
+//
 // 	b = a + b;
 // 	c = a * b;
-// 	
+//
 // 	document.writeln(c);
 // }
